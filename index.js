2f5fe19367bf0351c2ddcdd77d7f9cd459af0ec5
oladimeji-ojo <ojokuredim@gmail.com>
2020-04-07 23:00:20 +0100

    set up nodemailer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const PORT = process.env.PORT || 500;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 const express = require("express");
 const helmet = require("helmet");
@@ -10,10 +11,41 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use("/", (req, res) => {
-  res.send("<h1> Works </h1>");
-});
+// server.use("/", (req, res) => {
+//   res.send("<h1> Working... </h1>");
+// });
 
-// server.use("/email", (req, res) => {});
+server.post("/email", (req, res) => {
+  const { senderemail, message, subject } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_ADDRESS,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: senderemail,
+    to: "ojokuredim@gmail.com",
+    subject,
+    message,
+  };
+
+  transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      res.status(500).json({
+        error,
+        message: `sending email failed!`,
+      });
+    } else {
+      res.status(200).json({
+        response,
+        message: "email sent successfully",
+      });
+    }
+  });
+});
 
 module.exports = server;
