8b5d6290d360345b681078919e59c060a6ec02e0
oladimeji-ojo <ojokuredim@gmail.com>
2020-04-10 17:08:16 +0100

    finishing

--- middleware/validateContent.js
+++ middleware/validateContent.js
@@ -2,15 +2,15 @@ module.exports = (req, res, next) => {
   let content = req.body;
   if (!content) {
     res.status(400).json({
-      message: "Ooops! kindly populate the fields",
+      message: "Ooops! kindly populate the fields 🥺",
     });
   } else if (!content.senderemail) {
     res.status(400).json({
-      message: "I'd love to write you back, kindly input your email",
+      message: "I'd love to write you back, kindly input your email 🥺",
     });
   } else if (!content.message) {
     res.status(400).json({
-      message: "Oops! kindly type in a message",
+      message: "Oops! kindly type in a message 🥺",
     });
   } else {
     next();
